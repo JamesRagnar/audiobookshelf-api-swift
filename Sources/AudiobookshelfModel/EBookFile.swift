c286c1a888df8589246baa94c313fd9d3fc892b8
James Harquail <JamesRagnar@users.noreply.github.com>
2024-12-22 20:04:53 -0500

    Update EBookFile.swift

--- Sources/AudiobookshelfModel/EBookFile.swift
+++ Sources/AudiobookshelfModel/EBookFile.swift
@@ -9,6 +9,21 @@ import Foundation
 
 public struct EBookFile {
     
+    /// The inode of the ebook file.
+    public let ino: String
+    
+    /// The metadata of the ebook file.
+    public let metadata: FileMetadata
+    
+    /// The ebook format of the ebook file.
+    public let ebookFormat: String
+    
+    /// The time (in ms since POSIX epoch) when the library file was added.
+    public let addedAt: Int
+    
+    /// The time (in ms since POSIX epoch) when the library file was last updated.
+    public let updatedAt: Int
+    
 }
 
 extension EBookFile: Decodable {}
