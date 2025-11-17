9ffffc16bbb945d8231fcc04e055d6abb31e9949
James Harquail <JamesRagnar@users.noreply.github.com>
2025-11-17 14:50:11 -0500

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/JamesRagnar/RagnarNetworking",
-            branch: "interceptor"
+            branch: "main"
         )
     ],
     targets: [
